41efd07453c71b3b78c40cc36d59cd39342ecfd3
kanata <y_tanaka317@yahoo.co.jp>
2018-10-15 15:54:18 -0400

    Initial commit

---
+++ src/main/java/money/Money.java
@@ -0,0 +1,36 @@
+package money;
+
+public class Money {
+    protected int amount;
+    protected String currency;
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+    Money times(int multiplier) {
+        return new Money(this.amount * multiplier, currency);
+    };
+    public String currency() {
+        return this.currency;
+    }
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+
+    }
